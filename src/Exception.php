b7cb6399d602a60283204b25a94a118eb420fa28
serch3 <Wovive@gmail.com>
2025-01-26 22:28:10 -0600

    initial commit with composer setup and exception handling

---
+++ src/Exception.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bunny\Stream;
+
+class Exception extends \Exception
+{
+    public function __toString(): string
+    {
+        return __CLASS__.": {$this->message}\n";
+    }
+}
